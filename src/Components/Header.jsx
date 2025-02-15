c3f6695d26657e01365291a902e4fca78a5af316
jeffinmboaz <jeffinmboaz@gmail.com>
2025-02-15 22:43:40 +0530

    change in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,32 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Header() {
   return (
-    <Navbar expand="lg" fixed="top" bg="dark" variant="dark" className=" text-uppercase fw-bold">
-      <Container fluid >
-        <Navbar.Brand href="/"><h4>Imax</h4></Navbar.Brand>
+    <Navbar expand="lg" fixed="top" bg="dark" variant="dark" className="text-uppercase fw-bold">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/"><h4>Imax</h4></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0 " navbarScroll>
-            <Nav.Link Link to href="/movieapps" className='text-white '>Home</Nav.Link>
-            <Nav.Link  Link to href="/movies" className='text-white '>Movies</Nav.Link>
-            <Nav.Link Link to href="/tvshows" className='text-white '>Tv shows</Nav.Link>
-            <Nav.Link Link to href="/topmovies" className='text-white '>Top Movies</Nav.Link>
-            <Nav.Link Link to href="/topshows" className='text-white '>Top shows</Nav.Link>
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+            <Nav.Link as={Link} to="/tvshows">TV Shows</Nav.Link>
+            <Nav.Link as={Link} to="/topmovies">Top Movies</Nav.Link>
+            <Nav.Link as={Link} to="/topshows">Top Shows</Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Form.Control type="search" placeholder="Search movies, TV shows..." className="me-2 rounded-pill" aria-label="Search" />
+            <Form.Control type="search" placeholder="Search..." className="me-2 rounded-pill" />
             <Button variant="outline-info">Search</Button>
           </Form>
-         
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
+
 export default Header;
